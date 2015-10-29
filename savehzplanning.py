1a9ae91ba18635145b26f1a06893f057fe8e018a
filon2008 <filon2008@gmail.com>
2015-10-29 16:20:13 +0800

    fix import bug add arg support

--- savehzplanning.py
+++ savehzplanning.py
@@ -1,7 +1,16 @@
-import urllib
+import urllib.request
+import sys
+import os
+
 def savehzplanning(url):
     urls = url.split('image=')
     asciis = urls[1].split('%u')[1:]
     pmsg = ''.join([chr(int(x, 16)) for x in asciis])
     image_url = '/'.join(urls[0].split('/')[:3]) + pmsg
     urllib.request.urlretrieve(image_url, pmsg.split('/')[-1])
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("usage : %s url" % os.path.basename(sys.argv[0]))
+    else:
+        savehzplanning(sys.argv[1])
